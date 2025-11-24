a119d5b6763621f70415ad1e6c11e697d97477a9
Suworda <su5200su@gmail.com>
2025-11-24 18:17:43 +0800

    Create 610 . 邪惡水族箱.cpp

---
+++ 610 . 邪惡水族箱.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+string s;
+int id=0;
+const int M = 998244353;
+
+ll calc(){
+    cin>>s;
+    if(s == "f") return (2*calc() -3)%M;
+    else if(s == "g") return (2*calc() + calc() -7)%M;
+    else if(s == "h") return (3*calc() - 2*calc() + calc())%M;
+
+    return stoi(s);
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie();
+
+    int n;
+    cin>>n;
+
+    cout << (calc()+M)%M <<'\n';
+}
